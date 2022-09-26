1ee25a9922990cf0ad190d13cc1ce92e71d9d58b
pcheucle <34266958+pcheucle@users.noreply.github.com>
2022-09-26 09:02:08 +0200

    update site url and repo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://pcheucle.github.io/blog',
+  siteRepo: 'https://github.com/pcheucle/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
